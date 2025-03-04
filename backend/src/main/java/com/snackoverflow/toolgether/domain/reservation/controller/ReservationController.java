4dbf64516c2f6c833f32d1e326a77f43d41a239a
skvhffpdyd <115456416+skvhffpdyd@users.noreply.github.com>
2025-03-04 15:29:11 +0900

    ✨feat : 예약 Controller RsData 적용

--- backend/src/main/java/com/snackoverflow/toolgether/domain/reservation/controller/ReservationController.java
+++ backend/src/main/java/com/snackoverflow/toolgether/domain/reservation/controller/ReservationController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.snackoverflow.toolgether.domain.reservation.dto.ReservationRequest;
 import com.snackoverflow.toolgether.domain.reservation.dto.ReservationResponse;
 import com.snackoverflow.toolgether.domain.reservation.service.ReservationService;
+import com.snackoverflow.toolgether.global.dto.RsData;
 
 import lombok.RequiredArgsConstructor;
 
@@ -21,27 +22,36 @@ public class ReservationController {
 	private final ReservationService reservationService;
 
 	@PostMapping("/request")
-	public ReservationResponse createReservation(@RequestBody ReservationRequest reservationRequest) {
-		return reservationService.requestReservation(reservationRequest);
+	public RsData<ReservationResponse> createReservation(@RequestBody ReservationRequest reservationRequest) {
+		ReservationResponse response = reservationService.requestReservation(reservationRequest);
+		return new RsData<>("200-1", "예약 요청 성공", response);
 	}
 
 	@PostMapping("/{id}/approve")
-	public void approveReservation(@PathVariable Long id) {
+	public RsData<Void> approveReservation(@PathVariable Long id) {
 		reservationService.approveReservation(id);
+		return new RsData<>("200-1",
+			"%d번 예약 승인 성공".formatted(id));
 	}
 
 	@PatchMapping("/{id}/reject")
-	public void rejectReservation(@PathVariable Long id, @RequestParam String reason) {
+	public RsData<Void> rejectReservation(@PathVariable Long id, @RequestParam String reason) {
 		reservationService.rejectReservation(id, reason);
+		return new RsData<>("200-1",
+			"%d번 예약 거절 성공".formatted(id));
 	}
 
 	@PatchMapping("/{id}/start")
-	public void startRental(@PathVariable Long id) {
+	public RsData<Void> startRental(@PathVariable Long id) {
 		reservationService.startRental(id);
+		return new RsData<>("200-1",
+			"%d번 예약 대여 시작 성공".formatted(id));
 	}
 
 	@PatchMapping("/{id}/complete")
-	public void completeRental(@PathVariable Long id) {
+	public RsData<Void> completeRental(@PathVariable Long id) {
 		reservationService.completeRental(id);
+		return new RsData<>("200-5",
+			"%d번 예약 대여 종료 성공".formatted(id));
 	}
 }
