52906bd68fdac4b58088b8f7e52e79c20153c793
sun <suunn001@gmail.com>
2025-03-05 15:18:42 +0900

    :sparkles: feat: 회원가입 로직 구현

---
+++ backend/src/main/java/com/snackoverflow/toolgether/domain/user/service/UserService.java
@@ -0,0 +1,61 @@
+package com.snackoverflow.toolgether.domain.user.service;
+
+import com.snackoverflow.toolgether.domain.user.entity.User;
+import com.snackoverflow.toolgether.domain.user.dto.SignupRequest;
+import com.snackoverflow.toolgether.domain.user.repository.UserRepository;
+import com.snackoverflow.toolgether.global.exception.custom.duplicate.DuplicateFieldException;
+import com.snackoverflow.toolgether.global.exception.custom.mail.VerificationException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final VerificationService verificationService;
+
+    // 이메일, 아이디, 닉네임 중복 방지
+    public void checkDuplicates(SignupRequest request) {
+        if (userRepository.existsByUsername(request.getUsername())) {
+            throw new DuplicateFieldException("사용자 ID 중복 오류 발생");
+        }
+        if (userRepository.existsByEmail(request.getEmail())) {
+            throw new DuplicateFieldException("사용자 EMAIL 중복 오류 발생");
+        }
+        if (userRepository.existsByNickname(request.getNickname())) {
+            throw new DuplicateFieldException("사용자 닉네임 중복 오류 발생");
+        }
+    }
+
+    // 회원 가입
+    public void registerVerifiedUser(SignupRequest request) {
+        // 이메일 인증 완료 시 회원 가입 허용
+        if (!verificationService.isEmailVerified(request.getEmail())) {
+            throw new VerificationException(VerificationException.ErrorType.NOT_VERIFIED, "인증되지 않은 이메일입니다. 이메일: "+ request.getEmail());
+        }
+
+        // 비밀번호 암호화
+        String encodedPassword = passwordEncoder.encode(request.getPassword());
+
+        //User 엔티티 생성 후 DB 저장
+        User user = User.builder()
+                .username(request.getUsername())
+                .password(request.getPassword())
+                .email(request.getEmail())
+                .nickname(request.getNickname())
+                .address(request.getAddress())
+                .longitude(request.getLongitude())
+                .latitude(request.getLatitude())
+                .phoneNumber(request.getPhoneNumber())
+                .profileImage(null)
+                .build();
+
+        userRepository.save(user);
+    }
+
+}
