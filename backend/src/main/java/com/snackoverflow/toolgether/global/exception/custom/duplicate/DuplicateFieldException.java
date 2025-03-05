8d7c1622a2fe89d223fdbb7b1a4fd97fb0cb736d
sun <suunn001@gmail.com>
2025-03-05 15:17:53 +0900

    :sparkles: feat: 회원 가입 중복 방지 예외 처리 추가

---
+++ backend/src/main/java/com/snackoverflow/toolgether/global/exception/custom/duplicate/DuplicateFieldException.java
@@ -0,0 +1,7 @@
+package com.snackoverflow.toolgether.global.exception.custom.duplicate;
+
+public class DuplicateFieldException extends RuntimeException{
+    public DuplicateFieldException(String message) {
+        super(message);
+    }
+}
