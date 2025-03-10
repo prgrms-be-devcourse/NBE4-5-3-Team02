2ad0c3427fdbb6f1d8a6ae17f339b35bfea8c992
sun <suunn001@gmail.com>
2025-03-10 23:17:44 +0900

    ♻️ Refactor: 요청 DTO에 비밀번호를 체크하는 필드 추가

--- backend/src/main/java/com/snackoverflow/toolgether/domain/user/dto/request/SignupRequest.java
+++ backend/src/main/java/com/snackoverflow/toolgether/domain/user/dto/request/SignupRequest.java
@@ -11,6 +11,10 @@ public record SignupRequest(
         @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,}$", message = "비밀번호는 영문+숫자 조합 8자 이상이어야 합니다")
         String password,
 
+        @NotBlank(message = "비밀번호를 입력해주세요")
+        @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,}$", message = "비밀번호는 영문+숫자 조합 8자 이상이어야 합니다")
+        String checkPassword,
+
         @Email @NotBlank String email,
         @NotBlank String nickname,
         @NotBlank String phoneNumber,
