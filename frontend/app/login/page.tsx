4902951a41e43a5dfdfa438b41614f98df0ff0d6
sun <suunn001@gmail.com>
2025-03-10 23:52:28 +0900

    ♻️ Refactor: 프론트엔드 쿠키 전송 옵션 추가

--- frontend/app/login/page.tsx
+++ frontend/app/login/page.tsx
@@ -25,11 +25,11 @@ export default function LoginPage() {
         try {
             const response = await fetch('http://localhost:8080/api/v1/users/login', {
                 method: 'POST',
+                credentials: 'include',
                 headers: {
                     'Content-Type': 'application/json',
                 },
                 body: JSON.stringify({username, password}),
-                credentials: 'include' // 쿠키 전송 허용
             });
 
             const data = await response.json();
