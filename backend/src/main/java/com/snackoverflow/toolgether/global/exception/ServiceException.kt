22d2e3b128744991f48adc7871ebd12440259395
dlfjsld1 <ltk900216@gmail.com>
2025-04-09 11:27:56 +0900

    ServiceException 예외 처리 코드 코틀린 전환

--- backend/src/main/java/com/snackoverflow/toolgether/global/exception/ServiceException.kt
+++ backend/src/main/java/com/snackoverflow/toolgether/global/exception/ServiceException.kt
@@ -2,7 +2,6 @@ package com.snackoverflow.toolgether.global.exception
 
 import com.snackoverflow.toolgether.global.dto.RsData
 
-
 class ServiceException(code: String, message: String) : RuntimeException(message) {
     private val rsData: RsData<*> = RsData<Any>(code, message)
 
