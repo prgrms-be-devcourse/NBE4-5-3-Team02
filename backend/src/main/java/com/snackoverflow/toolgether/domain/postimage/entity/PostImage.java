32a9f521db14c96564a415b6720bcbaecf63dcb6
dlfjsld1 <ltk900216@gmail.com>
2025-04-09 11:27:36 +0900

    s3 관련 코드 코틀린 전환

--- backend/src/main/java/com/snackoverflow/toolgether/domain/postimage/entity/PostImage.java
+++ backend/src/main/java/com/snackoverflow/toolgether/domain/postimage/entity/PostImage.java
@@ -1,29 +1,29 @@
-package com.snackoverflow.toolgether.domain.postimage.entity;
+package com.snackoverflow.toolgether.domain.postimage.entity
 
-import com.snackoverflow.toolgether.domain.post.entity.Post;
-import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import com.snackoverflow.toolgether.domain.post.entity.Post
+import jakarta.persistence.*
+import lombok.AllArgsConstructor
+import lombok.Builder
+import lombok.Getter
+import lombok.NoArgsConstructor
 
 @Entity
 @Getter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class PostImage {
-
+class PostImage {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    var id: Long? = null
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "post_id", nullable = false)
-    private Post post;
+    var post: Post? = null
 
     @JoinColumn(nullable = false) // 한 장 이상 필수
-    private String imageUrl; // 이미지 파일 이름, uuid로 저장 (기존 postImage -> imageUrl 변경)
+    var imageUrl: String? = null // 이미지 파일 이름, uuid로 저장 (기존 postImage -> imageUrl 변경)
+
 
     /* TODO: 코틀린 변환 전 임시 게터. 추후 제거하고 사용하는 부분 수정 */
     public String getImageUrl() {
--- backend/src/main/java/com/snackoverflow/toolgether/global/exception/ServiceException.kt
+++ backend/src/main/java/com/snackoverflow/toolgether/global/exception/ServiceException.kt
@@ -2,6 +2,7 @@ package com.snackoverflow.toolgether.global.exception
 
 import com.snackoverflow.toolgether.global.dto.RsData
 
+
 class ServiceException(code: String, message: String) : RuntimeException(message) {
     private val rsData: RsData<*> = RsData<Any>(code, message)
 
