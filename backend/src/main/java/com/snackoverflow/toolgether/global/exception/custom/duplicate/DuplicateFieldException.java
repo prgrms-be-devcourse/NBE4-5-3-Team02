57344698c315a560c3f9aed816d57e642781fe66
sun <suunn001@gmail.com>
2025-04-10 17:24:10 +0900

    ♻️ Refactor: 불필요한 커스텀 오류를 삭제

--- backend/src/main/java/com/snackoverflow/toolgether/global/exception/custom/duplicate/DuplicateFieldException.java
+++
@@ -1,7 +0,0 @@
-package com.snackoverflow.toolgether.global.exception.custom.duplicate;
-
-public class DuplicateFieldException extends RuntimeException{
-    public DuplicateFieldException(String message) {
-        super(message);
-    }
-}
