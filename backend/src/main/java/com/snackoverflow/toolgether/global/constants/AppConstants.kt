c16fe7562bd51c6cb75f92b869cc07b1ce1eb81e
sun <suunn001@gmail.com>
2025-04-10 13:56:11 +0900

    ✨ Feat: static 변수 모음 파일 생성

---
+++ backend/src/main/java/com/snackoverflow/toolgether/global/constants/AppConstants.kt
@@ -0,0 +1,14 @@
+package com.snackoverflow.toolgether.global.constants;
+
+object AppConstants {
+    // 토큰
+    const val REFRESH_TOKEN = "refresh_token"
+    const val REMEMBER_ME_TOKEN = "remember_me_token"
+
+    // 위도, 경도 암호화 유틸 클래스
+    const val ALGORITHM = "AES"
+    const val KEY_SIZE = 256
+
+    // 채팅 관련
+    const val PERSONAL_CHAT_PREFIX = "chat:"
+}
