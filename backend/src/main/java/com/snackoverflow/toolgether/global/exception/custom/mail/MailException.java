cdc332c59289f15d7f8b33826c33205df670de72
sun <suunn001@gmail.com>
2025-04-10 17:05:36 +0900

    ♻️ Refactor: 불필요한 커스텀 오류를 삭제, 오류 처리 수정

--- backend/src/main/java/com/snackoverflow/toolgether/global/exception/custom/mail/MailException.java
+++
@@ -1,8 +0,0 @@
-package com.snackoverflow.toolgether.global.exception.custom.mail;
-
-// 메일 예외 베이스 클래스
-public abstract class MailException extends RuntimeException {
-    public MailException(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
