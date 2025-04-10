7fabe78d62f9c8c94b4660d761b04b14d3c034dd
sun <suunn001@gmail.com>
2025-04-10 15:35:24 +0900

    ✨ Feat: Config 파일 추가 수정

---
+++ backend/src/main/java/com/snackoverflow/toolgether/global/config/WebConfig.kt
@@ -0,0 +1,25 @@
+package com.snackoverflow.toolgether.global.config;
+
+import com.snackoverflow.toolgether.global.filter.LoginUserArgumentResolver
+import lombok.RequiredArgsConstructor
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.method.support.HandlerMethodArgumentResolver
+import org.springframework.web.reactive.function.client.WebClient
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+@RequiredArgsConstructor
+class WebConfig(
+        private val loginUserArgumentResolver: LoginUserArgumentResolver
+) : WebMvcConfigurer {
+
+    override fun addArgumentResolvers(resolvers: MutableList<HandlerMethodArgumentResolver>) {
+        resolvers.add(loginUserArgumentResolver)
+    }
+
+    @Bean
+    fun webClient(builder: WebClient.Builder): WebClient {
+        return builder.build()
+    }
+}
