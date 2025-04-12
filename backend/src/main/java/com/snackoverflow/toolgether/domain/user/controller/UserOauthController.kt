dea471cf8eb0266af0548667409ed0aa7ecf2ca6
sun <suunn001@gmail.com>
2025-04-13 00:05:08 +0900

    :sparkles: Feat: UserOauthController to Kotlin

--- backend/src/main/java/com/snackoverflow/toolgether/domain/user/controller/UserOauthController.kt
+++ backend/src/main/java/com/snackoverflow/toolgether/domain/user/controller/UserOauthController.kt
@@ -1,134 +1,136 @@
-package com.snackoverflow.toolgether.domain.user.controller;
+package com.snackoverflow.toolgether.domain.user.controller
 
-import com.snackoverflow.toolgether.domain.user.dto.request.AdditionalInfoRequest;
-import com.snackoverflow.toolgether.domain.user.entity.User;
-import com.snackoverflow.toolgether.domain.user.service.OauthService;
-import com.snackoverflow.toolgether.domain.user.service.UserServiceV2;
-import com.snackoverflow.toolgether.global.dto.RsData;
-import com.snackoverflow.toolgether.global.filter.CustomUserDetails;
-import com.snackoverflow.toolgether.global.filter.Login;
-import com.snackoverflow.toolgether.global.token.JwtService;
-import com.snackoverflow.toolgether.global.token.TokenService;
-import jakarta.servlet.http.HttpServletResponse;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.google.common.collect.ImmutableMap
+import com.snackoverflow.toolgether.domain.user.dto.request.AdditionalInfoRequest
+import com.snackoverflow.toolgether.domain.user.service.OauthService
+import com.snackoverflow.toolgether.domain.user.service.UserServiceV2
+import com.snackoverflow.toolgether.global.dto.RsData
+import com.snackoverflow.toolgether.global.filter.CustomUserDetails
+import com.snackoverflow.toolgether.global.filter.Login
+import com.snackoverflow.toolgether.global.token.JwtService
+import com.snackoverflow.toolgether.global.token.TokenService
+import jakarta.servlet.http.HttpServletResponse
+import org.slf4j.Logger
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.validation.annotation.Validated
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PatchMapping
+import org.springframework.web.bind.annotation.RestController
+import java.io.IOException
 
-import java.io.IOException;
-import java.util.Map;
-
-@Slf4j
 @RestController
-@RequiredArgsConstructor
-public class UserOauthController {
-
-    @Value("${custom.dev.frontUrl}")
-    private String frontUrl;
-
-    @Value("${jwt.access_expiration}")
-    private Long access;
-
-    @Value("${jwt.refresh_expiration}")
-    private Long refresh;
-
-    private final OauthService oauthService;
-    private final TokenService tokenService;
-    private final UserServiceV2 userService;
-    private final JwtService jwtService;
+class UserOauthController(
+    @Value("\${custom.site.frontUrl}") private val frontUrl: String,
+    @Value("\${jwt.access_expiration}") private val access: Long,
+    @Value("\${jwt.refresh_expiration}") private val refresh: Long,
+    private val oauthService: OauthService,
+    private val tokenService: TokenService,
+    private val userService: UserServiceV2,
+    private val jwtService: JwtService,
+    private val log: Logger
+) {
 
     @GetMapping("/login/oauth2/code/google")
-    public RsData<?> handleSocialLogin(String authCode, HttpServletResponse response) {
-        Map<String, Object> tokens = oauthService.getTokens(authCode);
-        String accessToken = (String) tokens.get("access_token");
+    fun handleSocialLogin(
+        authCode: String,
+        response: HttpServletResponse
+    ): RsData<Any> {
+
+        // 토큰 가져오기
+        val accessToken = oauthService.getTokens(authCode)["access_token"] as String
 
         // 액세스 토큰으로 사용자 정보 가져오기
-        Map<String, Object> userInfo = oauthService.getUserInfo(accessToken);
-        String email = (String) userInfo.get("email");
-        log.info("userInfo = {}", userInfo);
+        val userInfo = oauthService.getUserInfo(accessToken) as Map<String, Any>
+        val email = userInfo["email"] as String
+        log.info("userInfo = {}", userInfo)
 
         // 사용자 존재 여부 체크
-        if (userService.existsUser(email)) {
-            return handleExistingUserLogin(email, response);
+        return if (userService.existsUser(email)) {
+            handleExistingUserLogin(email, response) // 기존에 존재하는 사용자는 로그인 처리
         } else {
-            return handleNewUserRegistration(userInfo, response);
+            handleNewUserRegistration(userInfo, response) // 존재하지 않은 사용자는 추가 정보 입력
         }
-
     }
 
-    // 휴대폰 검증 로직 -> VerifyController 사용하기
-
+    // 휴대폰 검증 로직 (VerifyController) 이후에 -> 추가 정보 업데이트
     @PatchMapping("/oauth/users/additional-info")
-    public RsData<?> updateAdditionalInfo(@Validated AdditionalInfoRequest request,
-                                          @Login CustomUserDetails userDetails) {
+    fun updateAdditionalInfo(
+        @Validated request: AdditionalInfoRequest,
+        @Login userDetails: CustomUserDetails
+    ): RsData<Any> {
 
-        oauthService.updateAdditionalInfo(userDetails.getUserEmail(), request);
+        oauthService.updateAdditionalInfo(userDetails.userEmail, request)
 
-        // 액세스 토큰 생성 후 바디로 전송
-        return new RsData<>(
-                "201-2",
-                "추가 정보가 등록되었습니다.",
-                null);
+        return RsData("201-2", "추가 정보가 등록되었습니다.")
     }
 
-    // jwt 토큰 추가 (access / refresh)
-    private void successLogin(String email, Long userId, HttpServletResponse response) {
-        String accessToken = tokenService.createTokenByEmailAndId(email, userId, access);
-        // accessToken -> 헤더에 저장
-        response.setHeader("Authorization", "Bearer " + accessToken);
+    // JWT 토큰 추가 (access / refresh)
+    private fun successLogin(
+        email: String,
+        userId: Long,
+        response: HttpServletResponse
+    ) {
+        val (accessToken, refreshToken) = listOf(
+            tokenService.createTokenByEmailAndId(email, userId, access),
+            tokenService.createTokenByEmailAndId(email, userId, refresh)
+        )
 
-        String refreshToken = tokenService.createTokenByEmailAndId(email, userId, refresh);
-        // refreshToken 는 세션 쿠키로 저장 (페이지 닫을 시에 만료)
-        jwtService.setJwtSessionCookie(refreshToken, response);
-
-        log.info("header 저장 토큰: {}, 쿠키 저장 토큰: {}", accessToken, refreshToken);
-    }
-
-    private RsData<?> handleExistingUserLogin(String email, HttpServletResponse response) {
-        User user = userService.getUserByEmail(email);
-
-        // 성공적인 로그인 처리
-        successLogin(email, user.getId(), response);
-
-        try {
-            response.sendRedirect(frontUrl);
-        } catch (IOException e) {
-            throw new RuntimeException(e.getMessage(), e.getCause());
+        response.apply {
+            setHeader("Authorization", "Bearer $accessToken") // accessToken -> 헤더에 저장
+            jwtService.setJwtSessionCookie(refreshToken, this) // refreshToken는 세션 쿠키로 저장 (페이지 닫을 시에 만료)
         }
 
-        return new RsData<>(
-                "200-1",
-                "기존 사용자 로그인 성공",
-                Map.of(
-                        "user_id", user.getId(),
-                        "nickname", user.getNickname()
-                )
-        );
+        log.info("header 저장 토큰: {}, 쿠키 저장 토큰: {}", accessToken, refreshToken)
     }
 
-    private RsData<?> handleNewUserRegistration(Map<String, Object> userInfo, HttpServletResponse response) {
-        User socialUser = oauthService.createSocialUser(userInfo);
+    private fun handleExistingUserLogin(
+        email: String,
+        response: HttpServletResponse
+    ): RsData<Any> {
+
+        val user = userService.getUserByEmail(email)
 
         // 성공적인 로그인 처리
-        successLogin((String) userInfo.get("email"), socialUser.getId(), response);
+        successLogin(email, user.id!!, response)
 
         try {
-            response.sendRedirect(frontUrl + "/additional-info");
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+            response.sendRedirect(frontUrl)
+        } catch (e: IOException) {
+            throw RuntimeException(e.message, e.cause)
         }
 
-        return new RsData<>(
-                "201-1",
-                "신규 회원 가입 완료 - 추가 정보 입력 필요",
-                Map.of(
-                        "additionalInfoRequired", socialUser.getAdditionalInfoRequired(),
-                        "user_id", socialUser.getId(),
-                        "nickname", socialUser.getNickname()
-                )
-        );
+        return RsData(
+            "200-1", "기존 사용자 로그인 성공",
+            mapOf(
+                "user_id" to user.id,
+                "nickname" to user.nickname
+            )
+        )
+    }
+
+    private fun handleNewUserRegistration(
+        userInfo: Map<String, Any>,
+        response: HttpServletResponse
+    ): RsData<Any> {
+
+        val socialUser = oauthService.createSocialUser(userInfo)
+
+        // 성공적인 로그인 처리
+        successLogin(userInfo["email"] as String, socialUser.id!!, response)
+
+        try {
+            response.sendRedirect("$frontUrl/additional-info")
+        } catch (e: IOException) {
+            throw RuntimeException(e)
+        }
+
+        return RsData(
+            "201-1", "신규 회원 가입 완료 - 추가 정보 입력 필요",
+            mapOf(
+                "user_id" to socialUser.id,
+                "nickname" to socialUser.nickname
+            )
+        )
     }
 }
+
